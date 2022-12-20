e8f845980ad696f432d46e3978904786b12ede2e
DABHI21 <jdabhi218@gmail.com>
2022-12-20 10:48:02 +0530

    Basic Concept

---
+++ Flitght-Time.py
@@ -0,0 +1,6 @@
+# Calculate the total amount of Flight Time 
+
+Distance =int( input("Enter the distance between two destinations:  "))
+Average = int(input("Enter the Avg speed of the Flight : "))
+Flight_Time=float(Distance/Average)
+print(Flight_Time)
