84d75ef6476cc0dbcdfa09a83b4679741d1bdb57
DABHI21 <jdabhi218@gmail.com>
2023-05-25 21:47:47 +0530

    mean

---
+++ Python for Data Science/Number of Vaccinations.py
@@ -0,0 +1,10 @@
+# Find the mean number of vaccinations of given data 
+# here calculation is (0*5 + 1*8 + 2*4 + 3*3)/20 
+
+vac_nums = [0,0,0,0,0,
+            1,1,1,1,1,1,1,1,
+            2,2,2,2,
+            3,3,3
+            ]
+print((8+8+9)/20)
+
