571df3fcf337ccbb0716d4cb0a84f6c5058cc6cf
DABHI21 <jdabhi218@gmail.com>
2023-05-28 17:18:37 +0530

    variation

--- Python for Data Science/Number of Vaccinations.py
+++ Python for Data Science/Number of Vaccinations.py
@@ -1,10 +1,15 @@
 # Find the mean number of vaccinations of given data 
 # here calculation is (0*5 + 1*8 + 2*4 + 3*3)/20 
-
+# Find the variation of the given data 
+import numpy as np
 vac_nums = [0,0,0,0,0,
             1,1,1,1,1,1,1,1,
             2,2,2,2,
             3,3,3
             ]
-print((8+8+9)/20)
+arr=np.array(vac_nums)
+# print((8+8+9)/20)
+mean=np.sum(arr)/arr.size
+variation=np.sum((arr-mean)**2)/arr.size
+print(variation)
 
